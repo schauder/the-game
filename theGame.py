7073ed1cd0432441477e30125524db849da6003f
Jens Schauder <jens@schauderhaft.de>
2024-07-25 16:40:14 +0200

    Game setup works

--- theGame.py
+++ theGame.py
@@ -1,8 +1,12 @@
-# This should go into a discard pile class
+from random import shuffle
+
+# TODO: This should go into a discard pile class
 discardPiles = [1, 1, 100, 100]
 directions = [1, 1, -1, -1]
 
+# TODO: This should go into a draw pile class
 cards = list(range(2, 100))
+shuffle(cards)
 
 validInput = False
 numberOfPlayers = None
@@ -10,8 +14,20 @@ while not validInput:
     numberOfPlayers = int(input("Give me the number of players between 2 and 5: "))
     validInput = numberOfPlayers >= 2 and numberOfPlayers <= 5
 
-print(discardPiles)
-print(directions)
-print(cards)
-print(numberOfPlayers)
+# print cards before drawing
+print("cards before drawing: " + cards.__str__())
+
+numberOfCards = 6
+
+hands = []
+for playerIndex in range(0, numberOfPlayers):
+    hands.append(cards[-numberOfCards:])
+    cards = cards[:-numberOfCards]
+
+print("discard piles" + discardPiles.__str__())
+print("directions: " + directions.__str__())
+print("cards: " + cards.__str__())
+print("numberOfPlayers: " + numberOfPlayers.__str__())
+print("numberOfCards: " + numberOfCards.__str__())
+print("hands: " + hands.__str__())
 
